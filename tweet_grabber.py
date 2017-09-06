fbe8468ef5f9bd753888802a8f1e61dafba09357
DaveAGreer <david.a.greer@ttu.edu>
2017-09-06 16:38:28 -0500

    Fixed Coordinate Formatting
    Coordinate and text of tweet now get written to a tsv file for eventual
    use in D3js's Choropleth graph (US map by county)

--- tweet_grabber.py
+++ tweet_grabber.py
@@ -4,30 +4,26 @@ from tweepy import OAuthHandler
 from tweepy import Stream
 from tweepy.streaming import StreamListener
 
-
-consumer_key = 'iSRtkvFpgktayh7zBDO5SmjV4'
-consumer_secret = 'ftam77i3aZtrknDrVWAsJzfI3WRD3V02o8gFzv73NU0A1sZb9K'
-access_token = '2737819997-eUJxIDNiSuHuQssQIOtWvWrfPTcg04Jwoxw3KCo'
-access_secret = '3Z4J9Bv0mWXBjEqG311NJorplyCq3KL7C5kMUzUlcqKPY'
+#put twitter credentials below
+#consumer_key =
+#consumer_secret =
+#access_token =
+#access_secret =
 
 auth = OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_secret)
 
 api = tweepy.API(auth)
 
-
-
-
 class MyListener(StreamListener):
     def on_data(self, data):
         try:
-            #This code grabs all tweets. If you want only tweets that have coordinates enabled,
-            #comment this code out and use the variation below
-            with open('twitterdata.json', 'a', newline='') as f:
-                f.write(data)
-                return True
+            #This code grabs all tweets and prints their data into twitterdata.json. If you want only tweets that have
+            # coordinates enabled, comment this code out and use the other variation
+            #with open('twitterdata.json', 'a', newline='') as f:
+                #f.write(data)
+                #return True
 
-            '''
             #If you want to only grab tweets where the user has coordinates enabled, use this code
             #Note: Tweets where the user does not have coordinates enabled will throw an exception and
             #      will be printed to the console. If it is enabled, the coordinates will be printed.
@@ -41,7 +37,7 @@ class MyListener(StreamListener):
                         print(coords)
                         print('\n')
                         return True
-            '''
+
 
         except BaseException as e:
             print("Error on_data: %s" % str(e))
@@ -52,4 +48,4 @@ class MyListener(StreamListener):
         return True
 
 twitter_stream = Stream(auth, MyListener())
-twitter_stream.filter(track=['#Trump', 'trump'])
+twitter_stream.filter(track=['irma'])
