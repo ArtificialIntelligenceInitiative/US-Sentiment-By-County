e659b157dce965048ec0b9c4db1f13db39c65ce6
DaveAGreer <david.a.greer@ttu.edu>
2017-09-10 10:18:23 -0500

    Added sample tweets and changed tweetinfo.py
    Sample Tweets folder contains ~6500 tweets and tweet coordinates.
    Modified tweetinfo.py to create separate .txt files.

--- tweet_grabber.py
+++ tweet_grabber.py
@@ -5,10 +5,10 @@ from tweepy import Stream
 from tweepy.streaming import StreamListener
 
 #put twitter credentials below
-#consumer_key =
-#consumer_secret =
-#access_token =
-#access_secret =
+consumer_key = 'iSRtkvFpgktayh7zBDO5SmjV4'
+consumer_secret = 'ftam77i3aZtrknDrVWAsJzfI3WRD3V02o8gFzv73NU0A1sZb9K'
+access_token = '2737819997-eUJxIDNiSuHuQssQIOtWvWrfPTcg04Jwoxw3KCo'
+access_secret = '3Z4J9Bv0mWXBjEqG311NJorplyCq3KL7C5kMUzUlcqKPY'
 
 auth = OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_secret)
@@ -18,16 +18,7 @@ api = tweepy.API(auth)
 class MyListener(StreamListener):
     def on_data(self, data):
         try:
-            #This code grabs all tweets and prints their data into twitterdata.json. If you want only tweets that have
-            # coordinates enabled, comment this code out and use the other variation
-            #with open('twitterdata.json', 'a', newline='') as f:
-                #f.write(data)
-                #return True
-
-            #If you want to only grab tweets where the user has coordinates enabled, use this code
-            #Note: Tweets where the user does not have coordinates enabled will throw an exception and
-            #      will be printed to the console. If it is enabled, the coordinates will be printed.
-
+            # Grabs all new tweets with coordinates enabled and prints the tweet info to a .json file.
             json_data = json.loads(data)
             if json_data['coordinates']:
                 coords = json_data["coordinates"]
@@ -40,12 +31,12 @@ class MyListener(StreamListener):
 
 
         except BaseException as e:
-            print("Error on_data: %s" % str(e))
-        return True
+            #print("Error on_data: %s" % str(e))
+            return True
 
     def on_error(self, status):
         print(status)
         return True
 
 twitter_stream = Stream(auth, MyListener())
-twitter_stream.filter(track=['irma'])
+twitter_stream.filter(track=['#irma', 'irma'])
--- tweetinfo.py
+++ tweetinfo.py
@@ -1,17 +1,19 @@
 import json
 
-#Program extracts the tweet and coordinates from the twitterdata.json file and prints them to a tsv file
-#This will eventually be changed to run sentiment analysis on the tweet and replace the tweet in the tsv file
-#with a numerical value corresponding to the sentiment of the tweet
+# Program extracts the tweet and coordinates from the twitterdata.json file and prints them to txt files
+# This will eventually be changed to run sentiment analysis on the tweet and give a numerical value
+# corresponding to the sentiment of the tweet. The coordinates need to be converted to county as well.
+
 with open('twitterdata.json', 'r', encoding='utf-8') as f:
     try:
-        for line in f:
-            tweet = json.loads(line)
-            with open('tweet_coordinates.tsv', 'a') as c:
-                longitude, latitude = (tweet["coordinates"]["coordinates"]) #extract coordinates from dictionary
-                coordinates = str(latitude) + "," + str(longitude) #put coordinates in correct order
-                text = str(tweet["text"]).replace("\n", " ").replace("\t", " ").replace("\r", "") #replace non-text
-                c.write(coordinates + "\t" + text + "\n") #write data to a .tsv file
-                print((coordinates + "\t" + text + "\n")) #print data to terminal
+        with open('tweet_coordinates.txt', 'a', encoding='utf-8') as c:
+            with open('tweet_text.txt', 'a', encoding='utf-8') as t:
+                for line in f:
+                    tweet = json.loads(line)
+                    longitude, latitude = (tweet["coordinates"]["coordinates"]) # extract coordinates from dictionary
+                    coordinates = str(latitude) + "," + str(longitude) # put coordinates in correct order
+                    text = str(tweet["text"]).replace("\n", " ").replace("\t", " ").replace("\r", "") # replace non-text
+                    c.write(coordinates + "\n")
+                    t.write(text + "\n")
     except BaseException as e:
         print("Error: %s" % str(e))
